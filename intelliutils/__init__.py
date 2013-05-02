84e7a122754ec7d61244d5dcb350bf54cbf0dfb2
Danish Mujeeb <danish.mujeeb@gmail.com>
2013-05-01 22:22:45 -0400

    Added None handling functions

--- intelliutils/__init__.py
+++ intelliutils/__init__.py
@@ -8,3 +8,23 @@ class DotDict(dict):
         return self.get(attr, None)
     __setattr__= dict.__setitem__
     __delattr__= dict.__delitem__
+
+def is_none(val):
+    """
+    Returns true if the value is to be considered as None. The notion
+    of None includes empty string or space
+    """
+    if val in [None, '', ' ']:
+        return True
+    else:
+        return False
+
+def is_not_none(val):
+    return not is_none(val)
+
+def drop_nones(vals):
+    """
+    Remove None or empty strings from a list
+    """
+    return filter(is_not_none, vals)
+    
